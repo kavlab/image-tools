df74fd473e105f6d1eb9df5da592ae396ed241f4
alexvk <alex@kavlab.ru>
2024-04-07 19:59:22 +0400

    add pdf converter

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('colors', views.colors_page, name='colors_page'),
     path('pdf', views.pdf_page, name='pdf_page'),
+    path('convert_to_pdf', views.convert_to_pdf, name='convert_to_pdf')
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import base64
 
-from django.shortcuts import render
+import img2pdf
+from django.shortcuts import HttpResponse, render
 
 from .tools import determine_colors, image_from_base64, rgb2hex
 
@@ -41,6 +42,8 @@ def colors_page(request):
 def pdf_page(request):
     if request.method == 'POST' and request.FILES.getlist('images'):
         images = request.FILES.getlist('images')
+        pdf_data = img2pdf.convert(images)
+        print(type(pdf_data))
     return render(
         request,
         'pdf.html',
@@ -48,3 +51,12 @@ def pdf_page(request):
             'page1_active': '',
             'page2_active': 'active',
         })
+
+
+def convert_to_pdf(request):
+    if request.method == 'POST' and request.FILES.getlist('images'):
+        images = request.FILES.getlist('images')
+        pdf_data = img2pdf.convert(images)
+        response = HttpResponse(pdf_data, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="document.pdf"'
+        return response
